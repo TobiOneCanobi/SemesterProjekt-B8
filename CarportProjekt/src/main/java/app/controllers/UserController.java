28c3ac2f84f235830682136c7a806bf2e603fc21
HenrikThunbo10 <henningthunbo@hotmail.com>
2024-05-03 11:50:34 +0200

    routing methods

--- CarportProjekt/src/main/java/app/controllers/UserController.java
+++ CarportProjekt/src/main/java/app/controllers/UserController.java
@@ -1,7 +1,10 @@
 package app.controllers;
 
+import app.exceptions.DatabaseException;
 import app.persistence.ConnectionPool;
+import app.persistence.UserMapper;
 import io.javalin.Javalin;
+import io.javalin.http.Context;
 
 public class UserController
 {
@@ -10,11 +13,47 @@ public class UserController
     {
         app.post("login", ctx -> login(ctx, connectionPool));
         app.get("logout", ctx -> logout(ctx));
-        app.post("createuser", ctx -> login(ctx, connectionPool));
+        app.post("createuser", ctx -> createuser(ctx, connectionPool));
         app.get("homepage", ctx -> ctx.render("homepage.html"));
 
     }
 
+    public static void login(Context ctx, ConnectionPool connectionPool)
+    {
+        String email = ctx.formParam("eMail");
+        String password = ctx.formParam("passWord");
+        try
+        {
+            User user = UserMapper.login(eMail, passWord, connectionPool);
+            if (user != null)
+            {
+                ctx.sessionAttribute("currentUser", user);
+                ctx.sessionAttribute("userEmail", eMail);
+                ctx.sessionAttribute("userRole", user.getRole());
 
+                ctx.redirect("/index");
+            } else
+            {
+                ctx.attribute("message", "noget er gået galt");
+                ctx.render("loginpage.html");
+            }
+        } catch (DatabaseException e)
+        {
+            // Handle database error
+            ctx.attribute("message", "forkert email eller password");
+            ctx.render("loginpage.html");
+        }
+    }
+
+    public static void logout(Context ctx)
+    {
+        ctx.req().getSession().invalidate();
+        ctx.redirect("/");
+    }
+
+    public static void createuser(Context ctx, ConnectionPool connectionPool)
+    {
+
+    }
 
 }

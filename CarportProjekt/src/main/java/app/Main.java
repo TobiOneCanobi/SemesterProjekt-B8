47f54c18ac28251d6ff46dc299eef33afabbfc75
Umair-230104 <cph-ut38@cphbusiness.dk>
2024-05-03 13:01:17 +0200

    fixet at databasen virker og rettet små fejl i ordermapper

--- CarportProjekt/src/main/java/app/Main.java
+++ CarportProjekt/src/main/java/app/Main.java
@@ -12,7 +12,7 @@ public class Main
 
     private static final String USER = "postgres";
     private static final String PASSWORD = "1234";
-    private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
+    private static final String URL = "jdbc:postgresql://159.65.120.138:5432/%s?currentSchema=public";
     private static final String DB = "carport";
     private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
 

d6e10e1986b360a78a8b2281281c25d4c59ca3ee
HenrikThunbo10 <henningthunbo@hotmail.com>
2024-05-03 11:09:44 +0200

    Creation of basic routes

--- CarportProjekt/src/main/java/app/controllers/UserController.java
+++ CarportProjekt/src/main/java/app/controllers/UserController.java
@@ -1,7 +1,19 @@
 package app.controllers;
 
+import app.persistence.ConnectionPool;
+import io.javalin.Javalin;
+
 public class UserController
 {
+
+    public static void addRoutes(Javalin app, ConnectionPool connectionPool)
+    {
+        app.post("login", ctx -> login(ctx, connectionPool));
+        app.get("logout", ctx -> logout(ctx));
+        app.post("createuser", ctx -> login(ctx, connectionPool));
+        app.get("homepage", ctx -> ctx.render("homepage.html"));
+
+    }
 
 
 

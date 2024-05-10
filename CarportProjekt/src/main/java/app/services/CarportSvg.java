1a0d70c190eb06a69ab09e8860bb5f94098bd054
Umair-230104 <cph-ut38@cphbusiness.dk>
2024-05-10 17:08:23 +0200

    små ændringer i svg filen

--- CarportProjekt/src/main/java/app/services/CarportSvg.java
+++ CarportProjekt/src/main/java/app/services/CarportSvg.java
@@ -1,19 +1,17 @@
 package app.services;
 
-public class CarportSvg
-{
+public class CarportSvg {
     private int width;
     private int length;
     private Svg innerSvg;
     private Svg outerSvg;
 
-    public CarportSvg(int width, int height)
-    {
+    public CarportSvg(int width, int length) {
         this.width = width;
         this.length = length;
-        outerSvg = new Svg(0, 0, "0 0 855 690", "75%" );
-        innerSvg = new Svg(40, 20, "0 0 600 780", "100%" );
-        innerSvg.addRectangle(0,0,600, 780, "stroke-width:1px; stroke:#000000; fill: #ffffff");
+        outerSvg = new Svg(0, 0, "0 0 " + (width + 255) + " " + (length + 150), "75%");
+        innerSvg = new Svg(40, 20, "0 0 " + width + " " + length, "100%");
+        innerSvg.addRectangle(0, 0, width, length, "stroke-width:1px; stroke:#000000; fill: #ffffff");
         addArrow();
         addText();
         addBeams();
@@ -23,47 +21,40 @@ public class CarportSvg
     }
 
     //Bjælker
-    private void addBeams(){
-        innerSvg.addRectangle(0,35,4.5, 780, "stroke-width:1px; stroke:#000000; fill: #ffffff");
-        innerSvg.addRectangle(0,565,4.5, 780, "stroke-width:1px; stroke:#000000; fill: #ffffff");
+    private void addBeams() {
+        innerSvg.addRectangle(0, 35, 4.5, length, "stroke-width:1px; stroke:#000000; fill: #ffffff");
+        innerSvg.addRectangle(0, width - 35, 4.5, length, "stroke-width:1px; stroke:#000000; fill: #ffffff");
     }
 
     //Spær
-    private void addRafters(){
-        for (double i = 0; i < 780; i+= 55.714)
-        {
-            innerSvg.addRafters(i, 0.0, 600, 4.5,"stroke:#000000; fill: #ffffff" );
+    private void addRafters() {
+        double spacing = 55.714;
+        for (double i = 0; i < length; i += spacing) {
+            innerSvg.addRectangle(i, 0.0, width, 4.5, "stroke:#000000; fill: #ffffff");
         }
     }
 
     //Stolper
-    private void addPost(){
-        for (double i = 80; i < 780; i+= 200)
-        {
-            innerSvg.addPosts(i, 32.5, 10, 10,"stroke:#000000; fill: #000000" );
-        }
-
-        for (double i = 80; i < 780; i+= 200)
-        {
-            innerSvg.addPosts(i, 562.3, 10, 10,"stroke:#000000; fill: #000000" );
+    private void addPost() {
+        double spacing = 200;
+        for (double i = spacing; i < length; i += spacing) {
+            innerSvg.addRectangle(i, 32.5, 10, 10, "stroke:#000000; fill: #000000");
+            innerSvg.addRectangle(i, width - 37.5, 10, 10, "stroke:#000000; fill: #000000");
         }
     }
 
-    public void addArrow()
-    {
-        outerSvg.addArrow(750, 20, 750, 550, "stroke:#000000; fill: #000000");
-        outerSvg.addArrow(40, 570, 735, 570, "stroke:#000000; fill: #000000");
+    public void addArrow() {
+        outerSvg.addArrow(length + 50, 20, length + 50, width - 50, "stroke:#000000; fill: #000000");
+        outerSvg.addArrow(40, width + 50, length - 45, width + 50, "stroke:#000000; fill: #000000");
     }
 
-    public void addText()
-    {
-        outerSvg.addText(760, 300, 90, "600");
-        outerSvg.addText(390, 590, 0, "780");
+    public void addText() {
+        outerSvg.addText(length + 60, width / 2, 90, String.valueOf(width));
+        outerSvg.addText(length / 2, width + 70, 0, String.valueOf(length));
     }
 
     @Override
-    public String toString()
-    {
+    public String toString() {
         return outerSvg.toString();
     }
 }

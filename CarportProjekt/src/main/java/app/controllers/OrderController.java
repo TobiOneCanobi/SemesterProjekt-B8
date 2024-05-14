ff20f118f650a8b6916ee6f0e345faf16878c309
peterjanas <pejanas@gmail.com>
2024-05-14 14:17:51 +0200

    masse lort

--- CarportProjekt/src/main/java/app/controllers/OrderController.java
+++ CarportProjekt/src/main/java/app/controllers/OrderController.java
@@ -7,6 +7,7 @@ import app.entities.User;
 import app.exceptions.DatabaseException;
 import app.persistence.ConnectionPool;
 import app.persistence.OrderMapper;
+import app.services.Calculator;
 import app.services.CarportSvg;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
@@ -18,12 +19,13 @@ import java.util.Locale;
 
 public class OrderController
 {
-    /*
+
     public static void addRoutes(Javalin app, ConnectionPool connectionPool)
     {
-        app.get("orderoverviewadmin", ctx -> orderOverviewAdmin(ctx, connectionPool));
-        app.get("orderoverviewcustomer", ctx -> orderOverviewCustomer(ctx, connectionPool));
-        app.post("CreateOrder", ctx -> CreateOrder(ctx, connectionPool));
+        app.get("adminoverview", ctx -> orderOverviewAdmin(ctx, connectionPool));
+        app.get("showPartsList", ctx -> showPartsList(ctx,connectionPool));
+       // app.get("orderoverviewcustomer", ctx -> orderOverviewCustomer(ctx, connectionPool));
+       // app.post("CreateOrder", ctx -> CreateOrder(ctx, connectionPool));
         app.get("designcarport", ctx -> showCarport(ctx));
     }
 
@@ -32,8 +34,8 @@ public class OrderController
     {
         try
         {
-            List<Order> orderList = OrderMapper.loadOrdersForAdmin(connectionPool);
-            ctx.attribute("orders", orderList);
+            List<Order> orderList = OrderMapper.getAllOrders(connectionPool);
+            ctx.attribute("orderList", orderList);
             ctx.render("adminoverview.html");
         } catch (Exception e)
         {
@@ -42,7 +44,7 @@ public class OrderController
             ctx.render("adminoverview.html");
         }
     }
-
+/*
     public static void orderOverviewCustomer(Context ctx, ConnectionPool connectionPool)
     {
         User user = ctx.sessionAttribute("currentUser");
@@ -59,8 +61,75 @@ public class OrderController
             ctx.render("custommeroverview.html");
         }
     }
+*/
+    private static void showPartsList(Context ctx, ConnectionPool connectionPool)
+    {
+        int orderId = Integer.parseInt(ctx.formParam("orderIdChosen"));
+        try
+        {
+            List<OrderItem> orderItemList = OrderMapper.getOrderItemsByOrderId(orderId,connectionPool);
+
+            if(orderItemList.isEmpty())
+            {
+                ctx.render("showpartslist.html");
+                return;
+            }
+         OrderItem orderItem = orderItemList.get(0);
+
+            ctx.attribute("width", orderItem.getOrder().getCarportWidth());
+            ctx.attribute("length", orderItem.getOrder().getCarportLength());
+            ctx.attribute("orderItemList",orderItemList);
+            ctx.render("showpartslist.html");
+
+        }
+        catch (DatabaseException e)
+        {
+            // måske også en numberformat exception??
+            throw new RuntimeException();
+        }
+    }
+
+private static void sendRequest(Context ctx, ConnectionPool connectionPool)
+{
+
+        int width = ctx.sessionAttribute("width");
+        int length = ctx.sessionAttribute("length");
+        Boolean installationFee = ctx.sessionAttribute("installationFee");
+        int status = 1;
+        int totalPrice = 19999;
+        User user = ctx.sessionAttribute("CurrentUser");
+        //User user = new User(100,"tester1","tester1","test",2600,87654321,"testemail","testpassword","customer");
+        Order order = new Order(100, width, length,false, status, totalPrice, user);
+    try
+    {
+        order = OrderMapper.insertOrder(order,connectionPool);
+
+        //to do
+        // calculate order items (parts list)
+
+        Calculator calculator = new Calculator(width,length,connectionPool);
+        calculator.calcCarport(order);
 
 
+
+        // save parts in db
+
+
+        // create message to customer and render order / send confirmation
+        ctx.render("requestconfirmation.html");
+
+    }
+    catch (DatabaseException e)
+    {
+        throw new RuntimeException();
+
+    }
+
+
+
+}
+
+/*
     public static void CreateOrder(Context ctx, ConnectionPool connectionPool)
     {
         User currentUser = ctx.sessionAttribute("currentUser");
@@ -119,7 +188,7 @@ public class OrderController
         }
         return totalPrice;
     }
-
+*/
     public static void showCarport(Context ctx) {
         Locale.setDefault(Locale.US);
         try {
@@ -142,5 +211,5 @@ public class OrderController
     }
 
 
-     */
+
 }
